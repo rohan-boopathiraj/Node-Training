3955c075def7deeb229c7fd46511069c17fab572
rohan <rohan.boopathiraj@cdw.com>
2023-04-06 13:55:39 +0530

    Initial commit

--- Excercise1/index.js
+++ Excercise1/index.js
@@ -1,22 +1,18 @@
-let fs = require('fs')
-let colorPallete = require('./color_ palette.json')
+let fs = require("fs");
+let colorPallete = require("./color_ palette.json");
 
-let index = 0
-for(index = 0; index < 5; index++){
+let index = 0;
+let colors = [];
+for (index = 0; index < 5; index++) {
     let ind = Math.floor(Math.random() * colorPallete.length);
-
-    fs.appendFile("colorGenerated.txt", JSON.stringify(colorPallete[ind]) + "\n",(err)=>{
-        if(err)
-            console.log("Can't write to the File");
-        else
-            console.log(JSON.stringify(colorPallete[ind]));
-    })
+    colors.push(colorPallete[ind]);
 }
+fs.writeFile("colorGenerated.json", JSON.stringify(colors) + "\n", (err) => {
+    if (err) console.log("Can't write to the File");
+    else console.log(JSON.stringify(colorPallete[ind]));
+});
 
-    fs.readFile("colorGenerated.txt", "UTF-8", (err, data)=>{
-        if(err)
-            console.log("Can't read the file");
-        else    
-            console.log(data);
-    })
- 
+fs.readFile("colorGenerated.json", "UTF-8", (err, data) => {
+    if (err) console.log("Can't read the file");
+    else console.log(data);
+});
