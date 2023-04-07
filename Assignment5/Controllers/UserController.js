22c694d83b16f60017831ad6f27dbfc5085b4ed0
rohan <rohan.boopathiraj@cdw.com>
2023-04-07 19:21:29 +0530

    Initial commit

---
+++ Assignment5/Controllers/UserController.js
@@ -0,0 +1,67 @@
+let {
+    getBuddyService,
+    addBuddyService,
+    getBuddyByIdService,
+    updateBuddyService,
+    deleteBuddyService,
+} = require("../Services/UserService");
+let { writeFileSync, readFileSync } = require("../Utils/FileOperartions");
+let fs = require("fs");
+
+let filePath = "./cdw_ace23_buddies.json";
+let errorMsg = "File is not Accessible";
+let isAccessible = false;
+fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+        isAccessible = false;
+    } else {
+        isAccessible = true;
+    }
+});
+
+let addBuddy = (req, res) => {
+    if (isAccessible) {
+        const newBuddy = req.body;
+        res.send(addBuddyService(newBuddy));
+    } else {
+        res.send(errorMsg);
+    }
+};
+
+let getBuddy = (req, res) => {
+    if (isAccessible) {
+        res.send(getBuddyService());
+    } else {
+        res.send(errorMsg);
+    }
+};
+
+let getBuddyById = (req, res) => {
+    if (isAccessible) {
+        const id = req.params.id;
+        res.send(getBuddyByIdService(id));
+    } else {
+        res.send(errorMsg);
+    }
+};
+
+let updateBuddy = (req, res) => {
+    if (isAccessible) {
+        const updatedValue = req.body;
+        const id = req.params.id;
+        res.send(updateBuddyService(updatedValue, id));
+    } else {
+        res.send(errorMsg);
+    }
+};
+
+let deleteBuddy = (req, res) => {
+    if (isAccessible) {
+        const id = req.params.id;
+        res.send(deleteBuddyService(id));
+    } else {
+        res.send(errorMsg);
+    }
+};
+
+module.exports = { addBuddy, getBuddy, getBuddyById, updateBuddy, deleteBuddy };
---
+++ Assignment5/index.js
@@ -0,0 +1,22 @@
+let express = require("express");
+let fs = require("fs");
+let app = express();
+require("dotenv").config()
+
+// For parsing application/json 
+app.use(express.json());
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+let userRoute = require("./Routes/UserRoute");
+
+app.use("/user", userRoute);
+
+app.use("/", (req, res) => {
+    res.send("Base");
+});
+
+app.listen(process.env.PORT, () => {
+    let budddies = [];
+    fs.writeFileSync("cdw_ace23_buddies.json", JSON.stringify(budddies));
+});
