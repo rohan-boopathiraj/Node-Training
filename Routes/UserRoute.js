041e31c0cf2c924ca502b3ecf2b60b34f1dae0db
rohan-boopathiraj <125631952+rohan-boopathiraj@users.noreply.github.com>
2023-04-07 12:33:11 +0530

    Add files via upload

---
+++ Routes/UserRoute.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let router = express.Router();
+
+let userController = require("../Controllers/UserController");
+
+router.post("/", userController.addBuddy);
+
+router.get("/", userController.getBuddy);
+
+router.get("/:id", userController.getBuddyById);
+
+router.put("/:id", userController.updateBuddy);
+
+router.delete("/:id", userController.deleteBuddy);
+
+module.exports = router;
